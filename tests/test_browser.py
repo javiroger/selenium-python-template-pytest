3644b89eba323dfb2c8b9c0bf36534bc38c351d6
javiroger <javi.roger@orbitalads.com>
2020-11-08 17:09:47 +0100

    Refactor tests to new POM Architecture (pytest docs)

--- tests/test_browser.py
+++ tests/test_browser.py
@@ -1,13 +1,26 @@
 import pytest
 
-from page_objects.google_search_pom import GoogleSearchPage
-from page_objects.search_results_pom import SearchResultsPage
+from selenium import webdriver
+import page_objects.page as page
 
+class TestPythonOrgSearch():
+    """A sample test class to show how page object works"""
 
-class TestBrowser:
+    def test_search_in_python_org(self, browser):
+        """
+        Tests python.org search feature. Searches for the word "pycon" then verified that some results show up.
+        Note that it does not look for any particular text in search results page. This test verifies that
+        the results were not empty.
+        """
 
-    def test_example(self, browser):
-        googlesearchpage = GoogleSearchPage(browser)
-        searchresultspage = SearchResultsPage(browser)
-        googlesearchpage.searchfor("Selenium")
-        searchresultspage.link_selenium_present()
+        browser.get("http://www.python.org")
+        #Load the main page. In this case the home page of Python.org.
+        main_page = page.MainPage(browser)
+        #Checks if the word "Python" is in title
+        assert main_page.is_title_matches(), "python.org title doesn't match."
+        #Sets the text of search textbox to "pycon"
+        main_page.search_text_element = "pycon"
+        main_page.click_go_button()
+        search_results_page = page.SearchResultsPage(browser)
+        #Verifies that the results page is not empty
+        assert search_results_page.is_results_found(), "No results found."
